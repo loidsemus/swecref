2f07617fe3a3f00885a35557b5e11a7e4846964a
Emil <emil.j.carlen@gmail.com>
2019-11-16 22:34:33 +0100

    Update port to use sys env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http');
 const server = http.createServer(app)
-const port = 3000
+const port = process.env.PORT || 3000
 
 const schedule = require('node-schedule')
 const scrapeAndSave = require('./scrape')
